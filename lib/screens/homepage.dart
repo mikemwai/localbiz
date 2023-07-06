d0975741fcbbaf0a9e01c419e125e149411f9b47
MikeMwai <mwai.michael@strathmore.edu>
2023-07-06 11:29:25 +0300

    User Module: Foundation

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import '../utils/authentication.dart';
@@ -11,60 +13,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isDrawerOpen = false;
+
+  void toggleDrawer() {
+    setState(() {
+      isDrawerOpen = !isDrawerOpen;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LocalBiz'),
+        title: const Text('LocalBiz: Home Page'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Center(
+            //TODO: Google Maps Interface
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
               child: Text(
-                'Home Page',
+                'Profile',
                 style: TextStyle(
-                  fontSize: 30,
+                  color: Colors.white,
+                  fontSize: 24,
                 ),
               ),
             ),
-            const SizedBox(
-              height: 100,
+            ListTile(
+              title: Text('View Profile'),
+              onTap: () {
+                // TODO: Implement the profile screen navigation
+              },
             ),
-            Container(
-              padding: const EdgeInsets.only(bottom: 0),
-              width: MediaQuery.of(context).size.width,
-              height: 50,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.black45),
-                  ),
-                  onPressed: () {
-                    //sign out
-                    SchedulerBinding.instance.addPostFrameCallback((_) {
-                      Authentication.signout(context: context);
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => const Signin()));
-                    });
-                  },
-                  child: const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Sign out',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.white),
-                      ),
-                    ],
-                  )),
+            /*ListTile(
+              title: Text('Close Drawer'),
+              onTap(){ toggleDrawer;},
+            ),*/
+            const SizedBox(
+              height: 500,
+            ),
+            ListTile(
+              title: Text(
+                'Sign out',
+                style: TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+              onTap: () {
+                SchedulerBinding.instance!.addPostFrameCallback((_) {
+                  Authentication.signout(context: context);
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => const Signin(),
+                  ));
+                });
+              },
             ),
           ],
         ),
       ),
+      /*floatingActionButton: FloatingActionButton(
+        onPressed: toggleDrawer,
+        child: Icon(isDrawerOpen ? Icons.close : Icons.menu),
+      ),*/
     );
   }
 }
